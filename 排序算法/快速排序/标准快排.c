06085dc1e27cf2373b57421ae12b6adfe6cc0a5e
TOKdawn <dawninshadow0.0@gmail.com>
2018-05-30 15:39:50 +0800

    ..

--- 排序算法/快速排序/标准快排.c
+++ 排序算法/快速排序/标准快排.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include "iostream"
+using namespace std;
 /*
  快排的中心思想是在一段无序数的两头开始找大于基准数或小于基准数的值并且互换,
  直到两个指针相遇,再与基准数相换,使得基准数左的数都小于它,右边都大于它.
  */
 int QuickSort(int *Array,int left,int right);
 int main(int argc, char const *argv[]) {
-    int Array[5]={2,31,1,44,9};
-    QuickSort(Array,0,4);
-    for(int i=0;i<5;i++)
+    int Array[3]={12,19,14};
+    QuickSort(Array,0,2);
+    for(int i=0;i<3;i++)
         printf("%d ",Array[i]);
     return 0;
 }
 int QuickSort(int *Array,int left,int right){
+ 
     if(left>=right){
         return 0;//结束条件,left=right,表示两个数之间没有无序的数了
     }
